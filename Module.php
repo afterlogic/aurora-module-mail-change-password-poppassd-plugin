8f1a2420e4efd1006f77691b5abb5a23e4e60ce7
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:50:06 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\MailChangePasswordPoppassdPlugin;
 
 /**
+ * Allows users to change passwords on their email accounts using POPPASSD protocol.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
